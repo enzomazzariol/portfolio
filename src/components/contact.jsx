bd3eeed6ba93d57974b1e9811a6502985723ba42
mazzariolenzo <mazzariolenzo@gmail.com>
2025-01-21 17:27:59 +0100

    correccion uri del form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,7 +5,7 @@ export default function Contact() {
     <div className="flex flex-col mb-10 mx-auto">
       <div className="flex justify-center items-center">
         <form
-          action="https://getform.io/f/bnlldombs"
+          action="https://getform.io/f/bnlldomb"
           method="POST"
           data-netlify="true"
           className="flex flex-col w-full md:w-7/12"
@@ -16,7 +16,6 @@ export default function Contact() {
             name="name"
             placeholder="Name"
             className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
-            
           />
 
           <input
